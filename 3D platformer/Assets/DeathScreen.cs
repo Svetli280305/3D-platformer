0db2785e0a2720c99da32b549e8e62de4950c40b
Svetoslav Yotov <syotov2021@elam.co.uk>
2021-11-16 15:52:31 +0000

    death box

---
+++ 3D platformer/Assets/DeathScreen.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathScreen : MonoBehaviour
+{
+    public void Retry()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
